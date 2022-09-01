09cc592b3aba5568b59d077e42f7a9339f9114a1
Martin Stancanelli <mastanca14@gmail.com>
2022-09-01 15:23:46 +0100

    deprecate length in module

--- src/schema.js
+++ src/schema.js
@@ -64,13 +64,13 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in seconds"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
     "The module's video duration, in seconds"
-    durationInSeconds: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int
   }
 `;
 
